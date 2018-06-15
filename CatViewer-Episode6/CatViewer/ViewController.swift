9ae63e0eb03b76ba0e05690e576f0524d79e6f40
Josh Stephenson <josh@brightmediums.com>
2018-06-15 08:57:45 -0400

    rolling back to episode 6 state

--- CatViewer-Episode6/CatViewer/ViewController.swift
+++ CatViewer-Episode6/CatViewer/ViewController.swift
@@ -10,30 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var catButton: UIButton!
-    @IBOutlet weak var foxButton: UIButton!
+    @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    var image: UIImage? {
-        didSet {
-            self.imageView.image = image
-        }
-    }
-    
-    var active: Bool = true {
-        didSet {
-            if active {
-                self.activityIndicator.startAnimating()
-                self.catButton.isEnabled = false
-                self.foxButton.isEnabled = false
-            }else {
-                self.activityIndicator.stopAnimating()
-                self.catButton.isEnabled = true
-                self.foxButton.isEnabled = true
-            }
-        }
-    }
+    var image: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,28 +22,24 @@ class ViewController: UIViewController {
     }
     
     func downloadCat() {
-        self.active = true
+        self.activityIndicator.startAnimating()
+        self.refreshButton.isEnabled = false
+        
         let catURL = URL(string: "https://cataas.com/cat")
         
         DispatchQueue.global(qos: .userInitiated).async {
             let imageData = NSData(contentsOf: catURL!)
             DispatchQueue.main.async {
                 self.image = UIImage(data: imageData! as Data)
-                self.active = false
+                self.imageView.image = self.image
+                self.activityIndicator.stopAnimating()
+                self.refreshButton.isEnabled = true
             }
         }
     }
-    
-    func downloadFox() {
-        
-    }
 
-    @IBAction func didTapCatButton(_ sender: Any) {
+    @IBAction func didTapRefreshButton(_ sender: Any) {
         self.downloadCat()
-    }
-    
-    @IBAction func didTapFoxButton(_ sender: Any) {
-        self.downloadFox()
     }
     
     override func didReceiveMemoryWarning() {
