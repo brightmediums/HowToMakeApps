0adaa491ebc3975614cd2624f1162febd5773610
Josh Stephenson <josh@brightmediums.com>
2018-06-29 15:05:26 -0400

    tweaks

--- AudioScrubber/AudioScrubber/PaddingLabel.swift
+++ AudioScrubber/AudioScrubber/PaddingLabel.swift
@@ -12,16 +12,18 @@ class PaddingLabel: UILabel {
 
     @IBInspectable var leftInset: CGFloat = 10.0
     @IBInspectable var rightInset: CGFloat = 10.0
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
     
     override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets.init(top: 0.0, left: leftInset, bottom: 0.0, right: rightInset)
+        let insets = UIEdgeInsets.init(top: bottomInset, left: leftInset, bottom: bottomInset, right: rightInset)
         super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
     }
     
     override var intrinsicContentSize: CGSize {
         let size = super.intrinsicContentSize
         return CGSize(width: size.width + leftInset + rightInset,
-                      height: size.height + 0.0 + 0.0)
+                      height: size.height + topInset + bottomInset)
     }    
 
 }
--- AudioScrubber/AudioScrubber/ViewController.swift
+++ AudioScrubber/AudioScrubber/ViewController.swift
@@ -11,8 +11,35 @@ import SwiftyJSON
 
 let barWidth = 2
 let barSpacing = 1
-let bottomRatio = CGFloat(2.0/3.0)
+let topToBottomRatio = CGFloat(2.0/3.0)
 let waveformMargin = 30.0
+
+extension UIImage {
+    
+    func tint(with color: UIColor) -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, UIScreen.main.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return self }
+        
+        // flip the image
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.translateBy(x: 0.0, y: -self.size.height)
+        
+        // multiply blend mode
+        context.setBlendMode(.multiply)
+        
+        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        context.clip(to: rect, mask: self.cgImage!)
+        color.setFill()
+        context.fill(rect)
+        
+        // create UIImage
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
 
 class ViewController: UIViewController, UIScrollViewDelegate {
 
@@ -20,17 +47,44 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var waveformScrollView: UIScrollView!
     @IBOutlet weak var coverArtImageView: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
-    
     @IBOutlet weak var labelConstraint: NSLayoutConstraint!
+    var waveformImageView: UIImageView?
     var blurEffectView: UIVisualEffectView?
+    
+    /**
+     - Parameters active sets UI to active based on scrollview interaction
+     */
+    var active: Bool = false {
+        didSet {
+            if active {
+                self.blurEffectView?.isHidden = false
+                UIView.animate(withDuration: 0.15) {
+                    self.labelConstraint.constant = 360.0
+                    self.timeLabel.backgroundColor = UIColor.clear
+                    self.timeLabel.transform = CGAffineTransform(scaleX: 1.7, y: 1.7)
+                    self.view.layoutIfNeeded()
+                }
+            }else{
+                self.blurEffectView?.isHidden = true
+                UIView.animate(withDuration: 0.15) {
+                    self.labelConstraint.constant = 226.0
+                    self.timeLabel.backgroundColor = UIColor.black
+                    self.timeLabel.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                    self.view.layoutIfNeeded()
+                }
+                
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScrollView()
+        setupBlurView()
     }
     
     func setupScrollView(){
-        var json: JSON!
+        var json: JSON?
         if let path = Bundle.main.path(forResource: "waveform_data", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
@@ -42,81 +96,105 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             print("Invalid filename/path.")
         }
         
-        let samples = json["samples"].arrayValue
-        drawWaveForm(samples: samples, height: json["height"].intValue)
-    }
-    
-    private func drawWaveForm(samples: [JSON], height: Int) {
-        let containerHeight = CGFloat(height)
-        let containerWidth = CGFloat(samples.count * (barWidth + barSpacing)) + self.view.frame.size.width
-        print("count \(samples.count), containerWidth \(containerWidth), height \(containerHeight)")
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: containerWidth, height: containerHeight))
-        var count = 0
-        let img = renderer.image { ctx in
-            ctx.cgContext.setFillColor(UIColor.white.cgColor)
-            samples.forEach { (sample) in
-//            let sample = samples.first!
-                let topHeight = CGFloat(sample.intValue) * bottomRatio
-                let bottomHeight = CGFloat(sample.intValue) - topHeight
-            
-                let bottomY = containerHeight * bottomRatio + 1
-                let topY = containerHeight * bottomRatio - topHeight
-                
-                let xOffset = self.view.frame.size.width / 2.0
-                let x = CGFloat(count * (barWidth + barSpacing)) + xOffset
-                let width = CGFloat(barWidth)
-                
-                let top = CGRect(x: x, y: topY, width: width, height: topHeight)
-                ctx.cgContext.addRect(top)
-            
-                let bottom = CGRect(x: x, y: bottomY, width: width, height: bottomHeight)
-                ctx.cgContext.addRect(bottom)
-                count += 1
-            }
-            ctx.cgContext.drawPath(using: .fill)
+        if let samples = json?["samples"].arrayValue, let height = json?["height"].intValue {
+            drawWaveForm(samples: samples, height: height)
         }
-        let waveForm = UIImageView(image: img)
-        self.waveformScrollView.addSubview(waveForm)
-        self.waveformScrollView.contentSize = CGSize(width: containerWidth, height: self.waveformScrollView.frame.size.height)
-        self.coverArtScrollView.contentSize = CGSize(width: containerWidth, height: self.view.frame.size.height)
-        self.coverArtImageView.frame = CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: self.coverArtScrollView.contentSize)
     }
     
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-//            view.backgroundColor = .clear
-        
+    func setupBlurView() {
         if self.blurEffectView == nil{
             let blurEffect = UIBlurEffect(style: .dark)
             self.blurEffectView = UIVisualEffectView(effect: blurEffect)
-            //always fill the view
             self.blurEffectView!.frame = self.view.bounds
             self.blurEffectView!.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         }
         view.insertSubview(blurEffectView!, aboveSubview: self.coverArtScrollView)
-        UIView.animate(withDuration: 0.15) {
-            self.labelConstraint.constant = 360.0
-            self.timeLabel.backgroundColor = UIColor.clear
-            self.timeLabel.transform = CGAffineTransform(scaleX: 1.7, y: 1.7)
-            self.view.layoutIfNeeded()
+        self.blurEffectView?.isHidden = true
+    }
+    
+    private func drawWaveForm(samples: [JSON], height: Int) {
+        let containerHeight = CGFloat(height)
+        let containerWidth = CGFloat(samples.count * (barWidth + barSpacing))
+        
+        // Renderer will draw a static image for us
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: containerWidth, height: containerHeight))
+        
+        // We will keep a bar count so we can calculate the x position
+        var count = 0
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.white.cgColor)
+            samples.forEach { (sample) in
+                
+                /*
+                 Get the top bar height by multiplying the sample value by 2/3 and then the bottom is the remaining value
+                 */
+                let topHeight = CGFloat(sample.intValue) * topToBottomRatio
+                let bottomHeight = CGFloat(sample.intValue) - topHeight
+            
+                /*
+                 CoreGraphics uses an inverted Y axis so the initial Y of the bottom bar is our waveform "midline" set by the topToBottomRatio
+                 The initial Y of the top bar is the height of the top bar subtracted from the "midline"
+                 */
+                let bottomY = containerHeight * topToBottomRatio + 1
+                let topY = containerHeight * topToBottomRatio - topHeight
+                
+                /*
+                 Simply move each bar over by the bar's width plus the bar spacing
+                 */
+                let x = CGFloat(count * (barWidth + barSpacing))
+                let width = CGFloat(barWidth)
+                
+                // Make the top bar
+                let topBar = CGRect(x: x, y: topY, width: width, height: topHeight)
+                ctx.cgContext.addRect(topBar)
+            
+                // Make the bottom Bar
+                let bottomBar = CGRect(x: x, y: bottomY, width: width, height: bottomHeight)
+                ctx.cgContext.addRect(bottomBar)
+                
+                count += 1
+            }
+            ctx.cgContext.drawPath(using: .fill)
         }
+        // Waveform is returned as a static image and we display it here
+        self.waveformImageView = UIImageView(image: img)
+        self.waveformScrollView.addSubview(self.waveformImageView!)
+        
+        // Make sure the waveform scrollview content size matches the image size
+        self.waveformScrollView.contentSize = CGSize(width: containerWidth, height: self.waveformScrollView.frame.size.height)
+        
+        // Waveform starts at mid-screen (horizontally) when the audio starts, so we need some left/right padding (insets)
+        self.waveformScrollView.contentInset = UIEdgeInsetsMake(0, self.view.frame.size.width / 2.0, 0.0, self.view.frame.size.width / 2.0)
+        
+        // Cover Art is a square that fills the vertical area. Width then matcheds the height.
+        self.coverArtScrollView.contentSize = CGSize(width: self.view.frame.size.height, height: self.view.frame.size.height)
+        
+        // Scrollview should initially be scrolled to the left (into the inset)
+        self.waveformScrollView.setContentOffset(CGPoint(x: 0.0 - self.waveformScrollView.frame.size.width / 2.0, y: 0.0), animated: false)
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.active = true
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        self.blurEffectView?.removeFromSuperview()
-        UIView.animate(withDuration: 0.15) {
-            self.labelConstraint.constant = 226.0
-            self.timeLabel.backgroundColor = UIColor.black
-            self.timeLabel.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            self.view.layoutIfNeeded()
-        }
-        
+        self.active = false
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let point = scrollView.contentOffset
-        self.coverArtScrollView.setContentOffset(point, animated: false)
+        let newX: CGFloat
+        if point.x < 1.0 {
+            newX = 0.0
+        }else {
+            let factor = scrollView.contentSize.width / scrollView.frame.size.width 
+            newX = point.x / factor
+        }
+        let offset = CGPoint(x: newX, y: 0.0)
+        self.coverArtScrollView.setContentOffset(offset, animated: false)
     }
     
+    // to disable scrollview inertia
     func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
         self.waveformScrollView.setContentOffset(scrollView.contentOffset, animated: false)
     }
