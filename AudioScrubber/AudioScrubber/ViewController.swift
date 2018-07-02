01d187e9b20e05324f2b350dbe020a3f930b0f73
Josh Stephenson <josh@brightmediums.com>
2018-07-02 09:13:40 -0400

    refactoring / code cleanup

--- AudioScrubber/AudioScrubber/ViewController.swift
+++ AudioScrubber/AudioScrubber/ViewController.swift
@@ -9,50 +9,19 @@
 import UIKit
 import SwiftyJSON
 
-let barWidth = 2
-let barSpacing = 1
-let topToBottomRatio = CGFloat(2.0/3.0)
-let waveformMargin = 30.0
-
-extension UIImage {
-    
-    func tint(with color: UIColor) -> UIImage
-    {
-        UIGraphicsBeginImageContextWithOptions(self.size, false, UIScreen.main.scale)
-        guard let context = UIGraphicsGetCurrentContext() else { return self }
-        
-        // flip the image
-        context.scaleBy(x: 1.0, y: -1.0)
-        context.translateBy(x: 0.0, y: -self.size.height)
-        
-        // multiply blend mode
-        context.setBlendMode(.multiply)
-        
-        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
-        context.clip(to: rect, mask: self.cgImage!)
-        color.setFill()
-        context.fill(rect)
-        
-        // create UIImage
-        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-}
-
 class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var coverArtScrollView: UIScrollView!
-    @IBOutlet weak var waveformScrollView: UIScrollView!
+    @IBOutlet weak var waveformScrollView: WaveformView!
     @IBOutlet weak var coverArtImageView: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var labelConstraint: NSLayoutConstraint!
-    var waveformImageView: UIImageView?
     var blurEffectView: UIVisualEffectView?
     
     /**
-     - Parameters active sets UI to active based on scrollview interaction
+     - Parameters active sets UI to active:
+     -- cover art is blurred
+     -- time label is moved up, background color is removed and font size increased
      */
     var active: Bool = false {
         didSet {
@@ -79,26 +48,11 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupScrollView()
+        self.waveformScrollView.loadWaveform()
         setupBlurView()
-    }
-    
-    func setupScrollView(){
-        var json: JSON?
-        if let path = Bundle.main.path(forResource: "waveform_data", ofType: "json") {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
-                json = try JSON(data: data)
-            } catch let error {
-                print("parse error: \(error.localizedDescription)")
-            }
-        } else {
-            print("Invalid filename/path.")
-        }
         
-        if let samples = json?["samples"].arrayValue, let height = json?["height"].intValue {
-            drawWaveForm(samples: samples, height: height)
-        }
+        // Cover Art is a square that fills the vertical area. Width then matches the height.
+        self.coverArtScrollView.contentSize = CGSize(width: self.view.frame.size.height, height: self.view.frame.size.height)
     }
     
     func setupBlurView() {
@@ -107,70 +61,9 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             self.blurEffectView = UIVisualEffectView(effect: blurEffect)
             self.blurEffectView!.frame = self.view.bounds
             self.blurEffectView!.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            view.insertSubview(blurEffectView!, aboveSubview: self.coverArtScrollView)
         }
-        view.insertSubview(blurEffectView!, aboveSubview: self.coverArtScrollView)
         self.blurEffectView?.isHidden = true
-    }
-    
-    private func drawWaveForm(samples: [JSON], height: Int) {
-        let containerHeight = CGFloat(height)
-        let containerWidth = CGFloat(samples.count * (barWidth + barSpacing))
-        
-        // Renderer will draw a static image for us
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: containerWidth, height: containerHeight))
-        
-        // We will keep a bar count so we can calculate the x position
-        var count = 0
-        let img = renderer.image { ctx in
-            ctx.cgContext.setFillColor(UIColor.white.cgColor)
-            samples.forEach { (sample) in
-                
-                /*
-                 Get the top bar height by multiplying the sample value by 2/3 and then the bottom is the remaining value
-                 */
-                let topHeight = CGFloat(sample.intValue) * topToBottomRatio
-                let bottomHeight = CGFloat(sample.intValue) - topHeight
-            
-                /*
-                 CoreGraphics uses an inverted Y axis so the initial Y of the bottom bar is our waveform "midline" set by the topToBottomRatio
-                 The initial Y of the top bar is the height of the top bar subtracted from the "midline"
-                 */
-                let bottomY = containerHeight * topToBottomRatio + 1
-                let topY = containerHeight * topToBottomRatio - topHeight
-                
-                /*
-                 Simply move each bar over by the bar's width plus the bar spacing
-                 */
-                let x = CGFloat(count * (barWidth + barSpacing))
-                let width = CGFloat(barWidth)
-                
-                // Make the top bar
-                let topBar = CGRect(x: x, y: topY, width: width, height: topHeight)
-                ctx.cgContext.addRect(topBar)
-            
-                // Make the bottom Bar
-                let bottomBar = CGRect(x: x, y: bottomY, width: width, height: bottomHeight)
-                ctx.cgContext.addRect(bottomBar)
-                
-                count += 1
-            }
-            ctx.cgContext.drawPath(using: .fill)
-        }
-        // Waveform is returned as a static image and we display it here
-        self.waveformImageView = UIImageView(image: img)
-        self.waveformScrollView.addSubview(self.waveformImageView!)
-        
-        // Make sure the waveform scrollview content size matches the image size
-        self.waveformScrollView.contentSize = CGSize(width: containerWidth, height: self.waveformScrollView.frame.size.height)
-        
-        // Waveform starts at mid-screen (horizontally) when the audio starts, so we need some left/right padding (insets)
-        self.waveformScrollView.contentInset = UIEdgeInsetsMake(0, self.view.frame.size.width / 2.0, 0.0, self.view.frame.size.width / 2.0)
-        
-        // Cover Art is a square that fills the vertical area. Width then matcheds the height.
-        self.coverArtScrollView.contentSize = CGSize(width: self.view.frame.size.height, height: self.view.frame.size.height)
-        
-        // Scrollview should initially be scrolled to the left (into the inset)
-        self.waveformScrollView.setContentOffset(CGPoint(x: 0.0 - self.waveformScrollView.frame.size.width / 2.0, y: 0.0), animated: false)
     }
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
@@ -181,6 +74,10 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         self.active = false
     }
     
+    /*
+     When the waveform scrollview scrolls, use it's relative position to update
+     the position of the cover art scroll view
+     */
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let point = scrollView.contentOffset
         let newX: CGFloat
@@ -190,8 +87,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             let factor = scrollView.contentSize.width / scrollView.frame.size.width 
             newX = point.x / factor
         }
-        let offset = CGPoint(x: newX, y: 0.0)
-        self.coverArtScrollView.setContentOffset(offset, animated: false)
+        let coverArtOffset = CGPoint(x: newX, y: 0.0)
+        self.coverArtScrollView.setContentOffset(coverArtOffset, animated: false)
     }
     
     // to disable scrollview inertia
