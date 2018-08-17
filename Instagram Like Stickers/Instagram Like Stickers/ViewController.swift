8b73b61bdea6895755f2a4b80c18238b7bcb4d4f
Josh Stephenson <josh@brightmediums.com>
2018-08-17 12:29:01 -0400

    migrate to images for stickers instead of labels

--- Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
+++ Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 let maximumScale = CGFloat(1.1)
 let minimumScale = CGFloat(0.9)
 
-class Sticker : UILabel {
+class Sticker : UIImageView {
     var constraintLeft: NSLayoutConstraint!
     var constraintTop: NSLayoutConstraint!
     var constraintHeight: NSLayoutConstraint!
@@ -21,44 +21,39 @@ class Sticker : UILabel {
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var stickerLion: Sticker!
-    @IBOutlet weak var stickerPanda: Sticker!
-    @IBOutlet weak var stickerKoala: Sticker!
+    @IBOutlet weak var stickerTaco: Sticker!
+    @IBOutlet weak var stickerFlower: Sticker!
+    @IBOutlet weak var stickerMonkey: Sticker!
     
-    @IBOutlet weak var constraintLionTop: NSLayoutConstraint!
-    @IBOutlet weak var constraintLionLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintLionHeight: NSLayoutConstraint!
-    @IBOutlet weak var constraintPandaTop: NSLayoutConstraint!
-    @IBOutlet weak var constraintPandaLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintPandaHeight: NSLayoutConstraint!
-    @IBOutlet weak var constraintKoalaTop: NSLayoutConstraint!
-    @IBOutlet weak var constraintKoalaLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintKoalaHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintTacoTop: NSLayoutConstraint!
+    @IBOutlet weak var constraintTacoLeft: NSLayoutConstraint!
+    @IBOutlet weak var constraintTacoHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintFlowerTop: NSLayoutConstraint!
+    @IBOutlet weak var constraintFlowerLeft: NSLayoutConstraint!
+    @IBOutlet weak var constraintFlowerHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintMonkeyTop: NSLayoutConstraint!
+    @IBOutlet weak var constraintMonkeyLeft: NSLayoutConstraint!
+    @IBOutlet weak var constraintMonkeyHeight: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        [stickerLion, stickerPanda, stickerKoala].forEach { (sticker) in
-            if let sticker = sticker {
-                sticker.layer.cornerRadius = sticker.frame.size.width / 2.0
-            }
-        }
         
-        self.stickerLion.constraintLeft     = self.constraintLionLeft
-        self.stickerLion.constraintTop      = self.constraintLionTop
-        self.stickerPanda.constraintLeft    = self.constraintPandaLeft
-        self.stickerPanda.constraintTop     = self.constraintPandaTop
-        self.stickerKoala.constraintLeft    = self.constraintKoalaLeft
-        self.stickerKoala.constraintTop     = self.constraintKoalaTop
-        self.stickerLion.constraintHeight   = self.constraintLionHeight
-        self.stickerPanda.constraintHeight  = self.constraintPandaHeight
-        self.stickerKoala.constraintHeight  = self.constraintKoalaHeight
+        self.stickerTaco.constraintLeft     = self.constraintTacoLeft
+        self.stickerTaco.constraintTop      = self.constraintTacoTop
+        self.stickerFlower.constraintLeft    = self.constraintFlowerLeft
+        self.stickerFlower.constraintTop     = self.constraintFlowerTop
+        self.stickerMonkey.constraintLeft    = self.constraintMonkeyLeft
+        self.stickerMonkey.constraintTop     = self.constraintMonkeyTop
+        self.stickerTaco.constraintHeight   = self.constraintTacoHeight
+        self.stickerFlower.constraintHeight  = self.constraintFlowerHeight
+        self.stickerMonkey.constraintHeight  = self.constraintMonkeyHeight
         
-        self.stickerLion.originBeforeDragging = CGPoint(x: self.stickerLion.constraintLeft.constant, y: self.stickerLion.constraintTop.constant)
+        self.stickerTaco.originBeforeDragging = CGPoint(x: self.stickerTaco.constraintLeft.constant, y: self.stickerTaco.constraintTop.constant)
         
-        self.stickerPanda.originBeforeDragging = CGPoint(x: self.stickerPanda.constraintLeft.constant, y: self.stickerPanda.constraintTop.constant)
+        self.stickerFlower.originBeforeDragging = CGPoint(x: self.stickerFlower.constraintLeft.constant, y: self.stickerFlower.constraintTop.constant)
         
-        self.stickerKoala.originBeforeDragging = CGPoint(x: self.stickerKoala.constraintLeft.constant, y: self.stickerKoala.constraintTop.constant)
+        self.stickerMonkey.originBeforeDragging = CGPoint(x: self.stickerMonkey.constraintLeft.constant, y: self.stickerMonkey.constraintTop.constant)
     }
     
     @IBAction func didPinchSticker(_ sender: Any) {
