24fc1d56db57dc78c4fb88e40e91c0fa486a82fe
Josh Stephenson <josh@brightmediums.com>
2018-08-17 13:55:47 -0400

    removing pinch gesture for now. Will cover it in the next episode

--- Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
+++ Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
@@ -14,9 +14,6 @@ let minimumScale = CGFloat(0.9)
 class Sticker : UIImageView {
     var constraintLeft: NSLayoutConstraint!
     var constraintTop: NSLayoutConstraint!
-    var constraintHeight: NSLayoutConstraint!
-    var originBeforeDragging: CGPoint!
-    var workingHeight: CGFloat!
 }
 
 class ViewController: UIViewController {
@@ -27,62 +24,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var constraintTacoTop: NSLayoutConstraint!
     @IBOutlet weak var constraintTacoLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintTacoHeight: NSLayoutConstraint!
     @IBOutlet weak var constraintFlowerTop: NSLayoutConstraint!
     @IBOutlet weak var constraintFlowerLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintFlowerHeight: NSLayoutConstraint!
     @IBOutlet weak var constraintMonkeyTop: NSLayoutConstraint!
     @IBOutlet weak var constraintMonkeyLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintMonkeyHeight: NSLayoutConstraint!
     
+    var twoFingersActive: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.stickerTaco.constraintLeft     = self.constraintTacoLeft
         self.stickerTaco.constraintTop      = self.constraintTacoTop
-        self.stickerFlower.constraintLeft    = self.constraintFlowerLeft
-        self.stickerFlower.constraintTop     = self.constraintFlowerTop
-        self.stickerMonkey.constraintLeft    = self.constraintMonkeyLeft
-        self.stickerMonkey.constraintTop     = self.constraintMonkeyTop
-        self.stickerTaco.constraintHeight   = self.constraintTacoHeight
-        self.stickerFlower.constraintHeight  = self.constraintFlowerHeight
-        self.stickerMonkey.constraintHeight  = self.constraintMonkeyHeight
-        
-        self.stickerTaco.originBeforeDragging = CGPoint(x: self.stickerTaco.constraintLeft.constant, y: self.stickerTaco.constraintTop.constant)
-        
-        self.stickerFlower.originBeforeDragging = CGPoint(x: self.stickerFlower.constraintLeft.constant, y: self.stickerFlower.constraintTop.constant)
-        
-        self.stickerMonkey.originBeforeDragging = CGPoint(x: self.stickerMonkey.constraintLeft.constant, y: self.stickerMonkey.constraintTop.constant)
+        self.stickerFlower.constraintLeft   = self.constraintFlowerLeft
+        self.stickerFlower.constraintTop    = self.constraintFlowerTop
+        self.stickerMonkey.constraintLeft   = self.constraintMonkeyLeft
+        self.stickerMonkey.constraintTop    = self.constraintMonkeyTop
     }
     
-    @IBAction func didPinchSticker(_ sender: Any) {
-        let recognizer = sender as! UIPinchGestureRecognizer
-        let sticker = recognizer.view as! Sticker
-        
-        if recognizer.state == .began {
-            sticker.workingHeight = sticker.constraintHeight.constant
-        }else if recognizer.state == .changed {
-            let scale = recognizer.scale //> 1.0 ? maximumScale : minimumScale
-            let newHeight = sticker.workingHeight * scale
-            print("scale \(scale), height \(sticker.workingHeight), newHeight \(newHeight)")
-            sticker.constraintHeight.constant = newHeight
-        }
-    }
+    // Used to track how much translation has already been accounted for while finger remains down
+    var adjustedTranslation = CGPoint(x: 0.0, y: 0.0)
     
     @IBAction func didDragSticker(_ sender: Any) {
         let recognizer = sender as! UIPanGestureRecognizer
-        let translation = recognizer.translation(in: self.view)
+        var translation = recognizer.translation(in: self.view)
         let sticker = recognizer.view as! Sticker
-        
         
         if recognizer.state == .began {
             self.view.bringSubview(toFront: sticker)
         }else if recognizer.state == .changed {
-            sticker.constraintLeft.constant = translation.x + sticker.originBeforeDragging.x
-            sticker.constraintTop.constant = translation.y + sticker.originBeforeDragging.y
+            // adjust the translation by the amount already moved
+            translation.x -= adjustedTranslation.x
+            translation.y -= adjustedTranslation.y
+            
+            // move the sticker by the remaining amount
+            sticker.constraintLeft.constant = sticker.constraintLeft.constant + translation.x
+            sticker.constraintTop.constant = sticker.constraintTop.constant + translation.y
+            
+            // ammend translation to account for total adjustment
+            adjustedTranslation.x += translation.x
+            adjustedTranslation.y += translation.y
         }else if recognizer.state == .ended {
-            sticker.originBeforeDragging = CGPoint(x: sticker.constraintLeft.constant, y: sticker.constraintTop.constant)
+            adjustedTranslation = CGPoint(x: 0.0, y: 0.0)
         }
         
     }
