22428c9a14b58499423afe155ac66aada1a4c1a2
Josh Stephenson <josh@brightmediums.com>
2018-08-19 10:53:48 -0400

    image tweaks and code cleanup

--- Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
+++ Instagram Like Stickers/Instagram Like Stickers/ViewController.swift
@@ -8,63 +8,67 @@
 
 import UIKit
 
-class Sticker : UIImageView {
-    var constraintLeft: NSLayoutConstraint!
+class Sticker: UIImageView {
     var constraintTop: NSLayoutConstraint!
+    var constraintLeft: NSLayoutConstraint!
 }
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var stickerMonkey: Sticker!
     @IBOutlet weak var stickerTaco: Sticker!
     @IBOutlet weak var stickerFlower: Sticker!
-    @IBOutlet weak var stickerMonkey: Sticker!
     
-    @IBOutlet weak var constraintTacoTop: NSLayoutConstraint!
-    @IBOutlet weak var constraintTacoLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintFlowerTop: NSLayoutConstraint!
-    @IBOutlet weak var constraintFlowerLeft: NSLayoutConstraint!
-    @IBOutlet weak var constraintMonkeyTop: NSLayoutConstraint!
     @IBOutlet weak var constraintMonkeyLeft: NSLayoutConstraint!
-    
-    var twoFingersActive: Bool = false
+    @IBOutlet weak var constraintMonkeyTop: NSLayoutConstraint!
+    @IBOutlet weak var constraintTacoLeft: NSLayoutConstraint!
+    @IBOutlet weak var constraintTacoTop: NSLayoutConstraint!
+    @IBOutlet weak var constraintFlowerLeft: NSLayoutConstraint!
+    @IBOutlet weak var constraintFlowerTop: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.stickerTaco.constraintLeft     = self.constraintTacoLeft
-        self.stickerTaco.constraintTop      = self.constraintTacoTop
-        self.stickerFlower.constraintLeft   = self.constraintFlowerLeft
-        self.stickerFlower.constraintTop    = self.constraintFlowerTop
-        self.stickerMonkey.constraintLeft   = self.constraintMonkeyLeft
-        self.stickerMonkey.constraintTop    = self.constraintMonkeyTop
+        self.stickerMonkey.constraintLeft = constraintMonkeyLeft
+        self.stickerMonkey.constraintTop = constraintMonkeyTop
+        
+        self.stickerFlower.constraintLeft = constraintFlowerLeft
+        self.stickerFlower.constraintTop = constraintFlowerTop
+        
+        self.stickerTaco.constraintLeft = constraintTacoLeft
+        self.stickerTaco.constraintTop = constraintTacoTop
     }
     
-    // Used to track how much translation has already been accounted for while finger remains down
+    // Used to track how much translation has already been accounted for while the finger remains down
     var adjustedTranslation = CGPoint(x: 0.0, y: 0.0)
     
-    @IBAction func didDragSticker(_ sender: Any) {
+    @IBAction func didPanSticker(_ sender: Any) {
         let recognizer = sender as! UIPanGestureRecognizer
-        var translation = recognizer.translation(in: self.view)
-        let sticker = recognizer.view as! Sticker
         
-        if recognizer.state == .began {
-            self.view.bringSubview(toFront: sticker)
-        }else if recognizer.state == .changed {
-            // adjust the translation by the amount already moved
-            translation.x -= adjustedTranslation.x
-            translation.y -= adjustedTranslation.y
+        if let sticker = recognizer.view as? Sticker {
+            var translation = recognizer.translation(in: self.view)
             
-            // move the sticker by the remaining amount
-            sticker.constraintLeft.constant = sticker.constraintLeft.constant + translation.x
-            sticker.constraintTop.constant = sticker.constraintTop.constant + translation.y
-            
-            // ammend translation to account for total adjustment
-            adjustedTranslation.x += translation.x
-            adjustedTranslation.y += translation.y
-        }else if recognizer.state == .ended {
-            adjustedTranslation = CGPoint(x: 0.0, y: 0.0)
+            if recognizer.state == .began {
+                self.view.bringSubview(toFront: sticker)
+            }else if recognizer.state == .changed {
+                translation.y -= adjustedTranslation.y
+                translation.x -= adjustedTranslation.x
+                
+                sticker.constraintTop.constant += translation.y
+                sticker.constraintLeft.constant += translation.x
+                
+                adjustedTranslation.y += translation.y
+                adjustedTranslation.x += translation.x
+            }else if recognizer.state == .ended {
+                adjustedTranslation = CGPoint(x: 0.0, y: 0.0)
+            }
         }
-        
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
-
