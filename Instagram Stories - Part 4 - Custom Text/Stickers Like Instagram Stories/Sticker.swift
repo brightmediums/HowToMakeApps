b7fd47d6f204c230099fd7ffb1cd8a6f52d19b73
Josh Stephenson <josh@brightmediums.com>
2018-10-01 09:27:29 -0400

    adding part 4, initial coding

---
+++ Instagram Stories - Part 4 - Custom Text/Stickers Like Instagram Stories/Sticker.swift
@@ -0,0 +1,70 @@
+//
+//  Sticker.swift
+//  Stickers Like Instagram Stories
+//
+//  Created by Joshua Stephenson on 10/1/18.
+//  Copyright © 2018 Bright Mediums. All rights reserved.
+//
+
+import UIKit
+
+
+let defaultLabelFont = UIFont(name: "Helvetica", size: defaultFontSize)
+
+class Sticker: UIView {
+    var appliedTranslation  = CGPoint(x: 0.0, y: 0.0)
+    var appliedScale        = CGFloat(1.0)
+    var appliedRotation     = CGFloat(0.0)
+    
+    var label: UILabel?
+    var text: String? {
+        didSet {
+            label = UILabel(frame: self.frame)
+            if let label = label {
+                label.text = text
+                label.font = defaultLabelFont
+                label.isUserInteractionEnabled = false
+                self.addSubview(label)
+            }
+        }
+    }
+    
+    // Placement: Used to track how much translation has already been applied while the finger remains down
+    var translation = CGPoint(x: 0.0, y: 0.0) {
+        didSet { self.updateTransform() }
+    }
+    
+    // Scale: Used to track how much scaling has already been applied
+    var scale = CGFloat(1.0) {
+        didSet { self.updateTransform() }
+    }
+    
+    // Rotation: Used to track how much rotation is applied while fingers are down
+    var rotation = CGFloat(0.0) {
+        didSet { self.updateTransform() }
+    }
+    
+    func saveScale() {
+        self.appliedScale = self.appliedScale * scale
+        self.scale = CGFloat(1.0)
+    }
+    
+    func saveTranslation() {
+        self.appliedTranslation.x += translation.x
+        self.appliedTranslation.y += translation.y
+        translation = CGPoint(x: 0.0, y: 0.0)
+    }
+    
+    func saveRotation() {
+        self.appliedRotation = rotation
+        rotation = CGFloat(0.0)
+    }
+    
+    private func updateTransform() {
+        let translationTransform    = CGAffineTransform(translationX: self.translation.x + appliedTranslation.x, y: self.translation.y + appliedTranslation.y)
+        let scaleTransform          = CGAffineTransform(scaleX: scale * appliedScale, y: scale * appliedScale)
+        let rotationTransform       = CGAffineTransform(rotationAngle: rotation + appliedRotation)
+        
+        self.transform = rotationTransform.concatenating(scaleTransform).concatenating(translationTransform)
+    }
+}
